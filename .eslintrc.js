9262e22b89293f79a842bf9d9d4eee21931a15ae
36t <saluta@gmail.com>
2022-06-18 22:08:52 +0900

    chore(eslintrc): update extends values.

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
     extends: [
         "eslint:recommended",
         "standard",
-        "plugin:prettier/recommended"
+        "prettier"
     ],
     parserOptions: {
         ecmaVersion: "latest",
